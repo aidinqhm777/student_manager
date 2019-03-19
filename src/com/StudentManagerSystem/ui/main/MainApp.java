48a481efdad11426485adb3b92c1ce2e48e146d6
SeniorHunter <you@example.com>
2019-03-19 15:40:11 +0330

    javaFx UI

--- src/com/StudentManagerSystem/ui/main/MainApp.java
+++ src/com/StudentManagerSystem/ui/main/MainApp.java
@@ -9,11 +9,55 @@
 package com.StudentManagerSystem.ui.main;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 public class MainApp {
+    @FXML
+    private BorderPane rootPane;
 
     @FXML
     private void initialize() {
+        try {
+            Parent managerHome = FXMLLoader.load(getClass().getResource("/com/StudentManagerSystem/ui/managerHome/managerHome.fxml"));
+            rootPane.setCenter(managerHome);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @FXML
+    private void homeButtonHandler(){
+        try {
+            Parent managerHome = FXMLLoader.load(getClass().getResource("/com/StudentManagerSystem/ui/managerHome/managerHome.fxml"));
+            rootPane.setCenter(managerHome);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    private void addStudentButtonHandler(){
+        try {
+            Parent studentPage = FXMLLoader.load(getClass().getResource("/com/StudentManagerSystem/ui/addStudent/addStudent.fxml"));
+            rootPane.setCenter(studentPage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    private void editStudentButtonHandler(){
+        //TODO
+    }
+
+    @FXML
+    private void addEditCourseButtonHandler(){
+        //TODO
     }
 }
