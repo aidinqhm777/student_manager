70b09297f255720c5f1091b2c4dcac14deed86a6
aydinahmady <aidinqhm777@gmail.com>
2019-03-31 21:06:20 +0430

    manager1

--- src/com/StudentManagerSystem/Controllers/StudentManagerPageController.java
+++ src/com/StudentManagerSystem/Controllers/StudentManagerPageController.java
@@ -9,6 +9,12 @@
 
 package com.StudentManagerSystem.Controllers;
 
+import com.StudentManagerSystem.Student;
+import com.StudentManagerSystem.Subject;
+import com.StudentManagerSystem.SystemManage;
+
+import java.io.IOException;
+
 public class StudentManagerPageController {
 
     public static void loadStudentsInformation(){}
@@ -17,12 +23,31 @@ public class StudentManagerPageController {
     public static void loadAllSubjects(){}
     public static void loadAllSubjectsOfFaculty(){}
     public static void updateInformation(){}
-    public static void addSubject(){}
-    public static void removeSubject(){}
-    public static void updateSubject(){}
-    public static void addStudent(){}
-    public static void removeStudent(){}
-    public static void updateStudent(){}
+    public static void addSubject(Subject subject) throws IOException, ClassNotFoundException {
+        SystemManage.setSubjectTmp(subject);
+        SystemManage.addSubject();
+    }
+    public static void removeSubject(Subject subject) throws IOException {
+        SystemManage.setSubjectTmp(subject);
+        SystemManage.removeSubject();
+    }
+    public static void updateSubject(Subject subject1 , Subject subject2) throws IOException {
+        SystemManage.setSubjectTmp(subject1);
+        SystemManage.setUpdatedSubjectTmp(subject2);
+        SystemManage.editSubject();
+    }
+    public static void addStudent(Student student) throws IOException{
+        SystemManage.setStudentTmp(student);
+        SystemManage.signupStudent();
+    }
+    public static void removeStudent(Student student) throws IOException{
+        SystemManage.setStudentTmp(student);
+        SystemManage.removeStudent();
+    }
+    public static void updateStudent(Student student) throws IOException{
+        SystemManage.setStudentTmp(student);
+        SystemManage.updateStudent();
+    }
 
 
 
