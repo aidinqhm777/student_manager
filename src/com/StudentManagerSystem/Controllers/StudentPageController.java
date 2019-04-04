f426af736bd8bc3f07e94c5b5140bbef864cb678
aydinahmady <aidinqhm777@gmail.com>
2019-04-04 14:33:45 +0430

    control4

--- src/com/StudentManagerSystem/Controllers/StudentPageController.java
+++ src/com/StudentManagerSystem/Controllers/StudentPageController.java
@@ -32,6 +32,66 @@ public class StudentPageController {
             this.subjectCode = subjectCode;
         }
     }
+    public class UpdatedInput{
+        String studentID;
+        String subjectID;
+        String updatedGpcode;
+        Enrollment updatedenrollment;
+
+        public int getStudentID() {
+            return Integer.parseInt(studentID);
+        }
+
+        public void setStudentID(String studentID) {
+            this.studentID = studentID;
+        }
+
+        public int getSubjectID() {
+            return Integer.parseInt(subjectID);
+        }
+
+        public void setSubjectID(String subjectID) {
+            this.subjectID = subjectID;
+        }
+
+        public int getUpdatedGpcode() {
+            return Integer.parseInt(updatedGpcode);
+        }
+
+        public void setUpdatedGpcode(String updatedGpcode) {
+            this.updatedGpcode = updatedGpcode;
+        }
+
+        public Enrollment getUpdatedenrollment() {
+            return updatedenrollment;
+        }
+
+        public void setUpdatedenrollment(Enrollment updatedenrollment) {
+            this.updatedenrollment = updatedenrollment;
+        }
+    }
+    public class SearchByStudent{
+        String studentID;
+
+        public int getStudentID() {
+            return Integer.parseInt(studentID);
+        }
+
+        public void setStudentID(String studentID) {
+            this.studentID = studentID;
+        }
+    }
+    public class SearchBySubject{
+        String subjectID;
+
+        public int getSubjectID() {
+            return Integer.parseInt(subjectID);
+        }
+
+        public void setSubjectID(String subjectID) {
+            this.subjectID = subjectID;
+        }
+    }
 
 
 //    course offering
@@ -48,19 +108,23 @@ public class StudentPageController {
         enrollment.setStudentID(information.getUniID());
         enrollment.setSubjectID(input.getSubjectID());
         SystemManage.setEnrollmentTmp(enrollment);
-        SystemManage.removeEnrollment();
+        SystemManage.removeEnrollment(enrollment);
     }
-    public static void updateSubjectGpCode(int studentid , int subjectid , Enrollment updated) throws Exception{
+    public static void updateSubjectGpCode(UpdatedInput updatedInput) throws Exception{
         EnrollmentSearcher enrollmentSearcher = new EnrollmentSearcher();
         enrollmentSearcher.setSearchByStudent(true);
+        int studentid = updatedInput.getStudentID();
+        int subjectid = updatedInput.getSubjectID();
         enrollmentSearcher.setStudentID(studentid);
         enrollmentSearcher.setSubjectID(subjectid);
+        Enrollment updated = updatedInput.getUpdatedenrollment();
         LinkedList<Enrollment> enrollents = SystemManage.searchEnrollment(enrollmentSearcher);
         Enrollment enrollment = enrollents.pop();
         SystemManage.editEnrollment(enrollment , updated);
     }
-    public static LinkedList showSubjectsInformation(int id) throws Exception{
+    public static LinkedList showSubjectsInformation(SearchByStudent searchByStudent) throws Exception{
          LinkedList subjects = new LinkedList<Subject>();
+         int id = searchByStudent.getStudentID();
         EnrollmentSearcher enrollmentSearcher = new EnrollmentSearcher();
         enrollmentSearcher.setSearchByStudent(true);
         enrollmentSearcher.setStudentID(id);
@@ -72,14 +136,16 @@ public class StudentPageController {
         }
         return subjects;
     }
-    public static LinkedList searchSubject(int id)throws IOException, ClassNotFoundException{
+    public static LinkedList searchSubject(SearchBySubject searchBySubject)throws IOException, ClassNotFoundException{
         SubjectSearcher subjectSearcher = new SubjectSearcher();
+        int id = searchBySubject.getSubjectID();
         subjectSearcher.setSearchById(true);
         subjectSearcher.setId(id);
         return SystemManage.searchSubject(subjectSearcher);
     }
-    public static void loadInformation(int uniID) throws IOException, ClassNotFoundException {
+    public static void loadInformation(SearchByStudent searchByStudent) throws IOException, ClassNotFoundException {
         //TODO sets properties to information
+        int uniID = searchByStudent.getStudentID();
         StudentSearcher searcher = new StudentSearcher();
         searcher.setUniID(uniID);
         searcher.setSearchByUniID(true);
