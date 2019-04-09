78407d3f93b34383b7adabf8a56fd1507781f734
Amir Hosein Shekari <engineer.shekari@gmail.com>
2019-04-09 17:47:47 +0430

    javaFx add remove student (#23)

--- src/com/StudentManagerSystem/Controllers/StudentManagerPageController.java
+++ src/com/StudentManagerSystem/Controllers/StudentManagerPageController.java
@@ -1,12 +1,3 @@
-
-/*
- * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
 package com.StudentManagerSystem.Controllers;
 
 import com.StudentManagerSystem.*;
@@ -151,5 +142,4 @@ public class StudentManagerPageController {
         SystemManage.setUpdatedStudentTmp(student2);
         SystemManage.updateStudent();
     }
-
 }
--- src/com/StudentManagerSystem/Main.java
+++ src/com/StudentManagerSystem/Main.java
@@ -1,26 +1,51 @@
-/*
- * created by @Amir
- */
-
 package com.StudentManagerSystem;
 
-import com.StudentManagerSystem.Btree.BPlusTree;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import javax.xml.crypto.Data;
-import java.io.File;
+
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.LinkedList;
 
-public class Main {
+public class Main extends Application {
 
+    @Override
+    public void start(Stage primaryStage){
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        try { SystemManage.loadProgram(); } catch (IOException | ClassNotFoundException e) {
+            try { SystemManage.saveProgram();}
+            catch (IOException e1) {
+                System.out.println("Problem in load the files, restart the program"); }
+        }
 
+        Parent root;
 
-
+        try {
+            root = FXMLLoader.load(getClass().getResource("/com/StudentManagerSystem/ui/login/login.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
+
+    @Override
+    public void stop(){
+        try {
+            SystemManage.saveProgram();
+        } catch (IOException e) {
+            e.printStackTrace();
+            //todo
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
