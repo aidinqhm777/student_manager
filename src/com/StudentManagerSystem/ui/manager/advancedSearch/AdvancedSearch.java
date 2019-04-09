751946132019a0a3a7869a9ccd71f2dc40d80e25
seniorHunter <engineer.shekari@gmail.com>
2019-04-09 22:41:26 +0430

    javaFx advanced search fixed

--- src/com/StudentManagerSystem/ui/manager/advancedSearch/AdvancedSearch.java
+++ src/com/StudentManagerSystem/ui/manager/advancedSearch/AdvancedSearch.java
@@ -1,12 +1,89 @@
-/*
- * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
-
 package com.StudentManagerSystem.ui.manager.advancedSearch;
 
+import com.StudentManagerSystem.Controllers.StudentManagerPageController;
+import com.StudentManagerSystem.Student;
+import com.StudentManagerSystem.ui.data.StudentData;
+import com.jfoenix.controls.JFXTextField;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import java.io.IOException;
+import java.util.LinkedList;
+
 public class AdvancedSearch {
+
+    @FXML
+    private JFXTextField name;
+    @FXML
+    private JFXTextField lastName;
+    @FXML
+    private JFXTextField ID;
+    @FXML
+    private TableView list;
+
+    @FXML
+    private  TableColumn<StudentData, String> nameCloumn;
+    @FXML
+    private  TableColumn<StudentData, String> lastNameCloumn;
+    @FXML
+    private  TableColumn<StudentData, String> uniIdCloumn;
+    @FXML
+    private  TableColumn<StudentData, String> IDCloumn;
+
+    private ObservableList<StudentData> personData = FXCollections.observableArrayList();
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    private void searchByIDHandler(){
+        try {
+            LinkedList<Student> s = StudentManagerPageController.searchStudent(null,null, Integer.parseInt(ID.getText()) ,0);
+
+            setDataToList(s);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            //todo
+        }
+    }
+
+    @FXML
+    private void searchByNameHandler(){
+        try {
+            LinkedList<Student> s = StudentManagerPageController.searchStudent(name.getText(), lastName.getText().equals("") ? null :  lastName.getText() , 0 ,0);
+
+            setDataToList(s);
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            //todo
+        }
+    }
+
+    @FXML
+    private void selectStudentHandler(){
+
+    }
+
+    void setDataToList(LinkedList<Student> s){
+        personData.clear();
+        for (Student student : s) {
+            personData.add(new StudentData(student));
+        }
+        nameCloumn.setCellValueFactory(cellData ->  cellData.getValue().nameProperty());
+        lastNameCloumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+        IDCloumn.setCellValueFactory(cellData -> cellData.getValue().idProperty());
+        uniIdCloumn.setCellValueFactory(cellData -> cellData.getValue().uniIdProperty());
+        list.setItems(personData);
+    }
+
+
+
 }
